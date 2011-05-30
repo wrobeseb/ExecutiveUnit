613a741769aca353684f2d2f85dc28189edcace5
git <WrobeSeb@.tp.gk.corp.tepenet>
2011-05-30 14:37:43 +0200

    Poprawki

--- src/main/java/pwr/tin/tip/sw/pd/eu/db/dao/impl/UnitDaoImpl.java
+++ src/main/java/pwr/tin/tip/sw/pd/eu/db/dao/impl/UnitDaoImpl.java
@@ -23,7 +23,7 @@ public class UnitDaoImpl extends HibernateDaoSupport implements IUnitDao {
 	public void save(Object obj) {
 		Transaction transaction = getSession().beginTransaction();
 		Unit unit = (Unit)obj;
-		SQLQuery query = getSession().createSQLQuery("INSERT INTO unit (id_unit, address_ip, last_update_dt, max_process_no, overload_flg, type) VALUES (?,?,?,?,?,?)");
+		SQLQuery query = getSession().createSQLQuery("INSERT INTO unit (id, id_unit, address_ip, last_update_dt, max_process_no, overload_flg, type) VALUES (nextval ('unit_seq'),?,?,?,?,?,?)");
 		query.setInteger(0, unit.getIdUnit());
 		query.setString(1, unit.getAddressIp());
 		query.setDate(2, unit.getLastUpdateDt());
@@ -36,21 +36,21 @@ public class UnitDaoImpl extends HibernateDaoSupport implements IUnitDao {
 
 	@Override
 	public void setOverload(Integer id) {
-		SQLQuery query = getSession().createSQLQuery("UPDATE unit SET overload_flg = true WHERE id_unit = ?");
+		SQLQuery query = getSession().createSQLQuery("UPDATE unit SET overload_flg = true WHERE id_unit = ? AND type = 1");
 		query.setInteger(0, id);
 		query.executeUpdate();
 	}
 
 	@Override
 	public void setFree(Integer id) {
-		SQLQuery query = getSession().createSQLQuery("UPDATE unit SET overload_flg = false WHERE id_unit = ?");
+		SQLQuery query = getSession().createSQLQuery("UPDATE unit SET overload_flg = false WHERE id_unit = ? AND type = 1");
 		query.setInteger(0, id);
 		query.executeUpdate();
 	}
 
 	@Override
 	public void ping(Integer id) {
-		SQLQuery query = getSession().createSQLQuery("UPDATE unit SET last_update_dt = ? WHERE id_unit = ?");
+		SQLQuery query = getSession().createSQLQuery("UPDATE unit SET last_update_dt = ? WHERE id_unit = ? AND type = 1");
 		query.setDate(0, DateTime.now());
 		query.setInteger(1, id);
 		query.executeUpdate();
@@ -58,7 +58,7 @@ public class UnitDaoImpl extends HibernateDaoSupport implements IUnitDao {
 
 	@Override
 	public void removeUnit(Integer id) {
-		SQLQuery query = getSession().createSQLQuery("DELETE FROM unit WHERE id_unit = ?");
+		SQLQuery query = getSession().createSQLQuery("DELETE FROM unit WHERE id_unit = ? AND type = 1");
 		query.setInteger(0, id);
 		query.executeUpdate();
 	}
